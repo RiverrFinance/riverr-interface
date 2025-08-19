68a6219f77d3126f37d521d9c496c5c1aca964ea
U~Iriamuzu <daviswill264@gmail.com>
2025-08-19 23:21:30 +0100

    svg corrected

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const config = {
   title: 'Riverr',
   tagline: 'Decentralized Trading Platform Documentation',
-  favicon: 'img/Logo.png',
+  favicon: 'static/img/Logo.svg',
   future: {
     v4: true, 
   },
